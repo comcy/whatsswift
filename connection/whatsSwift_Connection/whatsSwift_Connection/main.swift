a220fb332bbe4e70434a9367c4acc10530f4f6e2
Silfang <30147@ing212813mac.infotronik.htw-aalen.int>
2014-12-15 09:19:50 +0100

    init

---
+++ connection/whatsSwift_Connection/whatsSwift_Connection/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  whatsSwift_Connection
+//
+//  Created by Silfang, Christian on 15.12.14.
+//  Copyright (c) 2014 Silfang, Christian. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
