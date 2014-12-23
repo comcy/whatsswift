743dbac36a47fb48845fa9ffe6ff669fa208d576
Bauer <52635@ing212808mac.infotronik.htw-aalen.int>
2014-12-23 11:40:06 +0100

    bevor impf. connection

--- server/WhatsSWIFT_Server/Params.swift
+++ server/WhatsSWIFT_Server/Params.swift
@@ -19,13 +19,18 @@ import Foundation
 
 //client refresh time
 var client_refresh_time = 5.0 //sec
+
 //msg refresh time
 var msg_refresh_time = 2.5 //sec
+
 //max error count befor disconnect (refresh_time*max_error) = time sec
 var max_error:Int = 10
+
 //max number of simultanious connected clients
 var max_clients = 100
+
 //max count of log entries
 var max_log_entries:Int = 500
+
 //logfile location
 var logfile_location = "/tmp/"
